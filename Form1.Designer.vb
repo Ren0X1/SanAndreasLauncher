072357a5e7cc40a63106ffd7a64db9ae61779460
Alejandro <alex-zam-olv@hotmail.com>
2022-02-02 20:57:47 +0100

    Subida Inicial

---
+++ Form1.Designer.vb
@@ -0,0 +1,191 @@
+﻿Imports System.Net
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Launcher
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+    Private components As System.ComponentModel.IContainer
+
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Launcher))
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.gotoTS = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.Transparent
+        Me.Button2.BackgroundImage = CType(resources.GetObject("Button2.BackgroundImage"), System.Drawing.Image)
+        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Franklin Gothic Medium", 19.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.Transparent
+        Me.Button2.Location = New System.Drawing.Point(1074, 13)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(20, 17)
+        Me.Button2.TabIndex = 6
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.Transparent
+        Me.Button1.BackgroundImage = CType(resources.GetObject("Button1.BackgroundImage"), System.Drawing.Image)
+        Me.Button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.FlatAppearance.BorderSize = 0
+        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Franklin Gothic Medium", 19.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.Transparent
+        Me.Button1.Location = New System.Drawing.Point(1106, 11)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(17, 19)
+        Me.Button1.TabIndex = 6
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.Transparent
+        Me.Button3.BackgroundImage = CType(resources.GetObject("Button3.BackgroundImage"), System.Drawing.Image)
+        Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button3.FlatAppearance.BorderSize = 0
+        Me.Button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Franklin Gothic Medium", 19.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.Transparent
+        Me.Button3.Location = New System.Drawing.Point(911, 372)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(195, 82)
+        Me.Button3.TabIndex = 6
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Transparent
+        Me.Button4.BackgroundImage = CType(resources.GetObject("Button4.BackgroundImage"), System.Drawing.Image)
+        Me.Button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.Button4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button4.FlatAppearance.BorderSize = 0
+        Me.Button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Franklin Gothic Medium", 19.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.Transparent
+        Me.Button4.Location = New System.Drawing.Point(895, 333)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(0)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(227, 29)
+        Me.Button4.TabIndex = 6
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'gotoTS
+        '
+        Me.gotoTS.BackColor = System.Drawing.Color.Transparent
+        Me.gotoTS.BackgroundImage = CType(resources.GetObject("gotoTS.BackgroundImage"), System.Drawing.Image)
+        Me.gotoTS.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.gotoTS.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.gotoTS.FlatAppearance.BorderSize = 0
+        Me.gotoTS.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.gotoTS.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.gotoTS.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.gotoTS.ForeColor = System.Drawing.Color.Transparent
+        Me.gotoTS.Location = New System.Drawing.Point(866, 397)
+        Me.gotoTS.Margin = New System.Windows.Forms.Padding(0)
+        Me.gotoTS.Name = "gotoTS"
+        Me.gotoTS.Size = New System.Drawing.Size(28, 32)
+        Me.gotoTS.TabIndex = 9
+        Me.gotoTS.UseVisualStyleBackColor = False
+        '
+        'PictureBox1
+        '
+        Dim foto1 As String = "https://raw.githubusercontent.com/Ren0X1/VersionCheck/main/1.png"
+        Dim img As Image = GetImageFromURL(foto1)
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Location = New System.Drawing.Point(52, 105)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(508, 343)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        Me.PictureBox1.Image = img
+        Me.PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+        '
+        'Launcher
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(1152, 648)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.gotoTS)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.DoubleBuffered = True
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Launcher"
+        Me.Text = "San Andreas Roleplay Launcher"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Public Function _
+        GetImageFromURL(ByVal url As String) As Image
+        Dim retVal As Image = Nothing
+        Try
+            If Not String.IsNullOrWhiteSpace(url) Then
+                Dim req As System.Net.WebRequest = System.Net.WebRequest.Create(url.Trim)
+
+                Using request As System.Net.WebResponse = req.GetResponse
+                    Using stream As System.IO.Stream = request.GetResponseStream
+                        retVal = New Bitmap(System.Drawing.Image.FromStream(stream))
+                    End Using
+                End Using
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(String.Format("An error occurred:{0}{0}{1}",
+                                          vbCrLf, ex.Message),
+                                          "Exception Thrown",
+                                          MessageBoxButtons.OK,
+                                          MessageBoxIcon.Warning)
+
+        End Try
+
+        Return retVal
+
+    End Function
+
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents gotoTS As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,63 @@
+﻿Imports System.Net
+
+Public Class Launcher
+    Dim drag As Boolean
+    Dim mousex As Integer
+    Dim mousey As Integer
+
+    Private Sub Launcher_MouseDown(sender As Object, e As MouseEventArgs) Handles MyBase.MouseDown
+        drag = True
+        mousex = Cursor.Position.X - Me.Left
+        mousey = Cursor.Position.Y - Me.Top
+    End Sub
+
+    Private Sub Launcher_MouseMove(sender As Object, e As MouseEventArgs) Handles MyBase.MouseMove
+        If drag Then
+            Me.Top = Cursor.Position.Y - mousey
+            Me.Left = Cursor.Position.X - mousex
+        End If
+    End Sub
+
+    Private Sub Launcher_MouseUp(sender As Object, e As MouseEventArgs) Handles MyBase.MouseUp
+        drag = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim ws As New WebClient
+        Dim ip As String
+        ip = ws.DownloadString("https://raw.githubusercontent.com/Ren0X1/VersionCheck/main/ipSanAndreas.txt")
+        Shell("Explorer.exe " + ip)
+    End Sub
+    Dim brr As String = "2"
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Dim foto2 As String = "https://raw.githubusercontent.com/Ren0X1/VersionCheck/main/2.png"
+        Dim img2 As Image = GetImageFromURL(foto2)
+
+        Dim foto1 As String = "https://raw.githubusercontent.com/Ren0X1/VersionCheck/main/1.png"
+        Dim img As Image = GetImageFromURL(foto1)
+        If brr = "2" Then
+            PictureBox1.Image = img2
+            brr = "1"
+        Else
+            PictureBox1.Image = img
+            brr = "2"
+        End If
+    End Sub
+
+    Private Sub gotoTS_Click(sender As Object, e As EventArgs) Handles gotoTS.Click
+        Dim ws As New WebClient
+        Dim ts3 As String
+        ts3 = ws.DownloadString("https://raw.githubusercontent.com/Ren0X1/VersionCheck/main/ts3SanAndreas.txt")
+        Shell("Explorer.exe " + ts3)
+    End Sub
+
+End Class
